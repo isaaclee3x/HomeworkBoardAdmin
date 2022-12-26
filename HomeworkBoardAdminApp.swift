b3a1e452d835e6b17a25b883ce80b4674aed41de
isaaclee <75001370+isaaclee3x@users.noreply.github.com>
2022-12-26 18:56:31 +0800

    Initial commit

---
+++ HomeworkBoardAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkBoardAdminApp.swift
+//  HomeworkBoardAdmin
+//
+//  Created by Isaac Lee Jing Zhi on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkBoardAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
